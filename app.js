44906c97cafe7e670a4d0829c511ef0bec7e0e75
Leonardo Calindra <leonardodacostadasilva@hotmail.com>
2022-04-20 20:43:28 -0300

    Aula 11

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 import express from "express";
 import * as dotenv from "dotenv";
-import Usuarios from "./src/controllers/Usuarios.js"
+// import Usuarios from "./src/controllers/Usuarios.js"
+import DatabaseMetodos from "./src/utils/DatabaseMetodos.js";
 
 dotenv.config()
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 app.use(express.json())
 
-
+DatabaseMetodos.createTable()
 app.listen(port, ()=>{
     console.log(`Servidor em http://localhost:${port}`)
 })
 
 
-Usuarios.routers(app)
+// Usuarios.routers(app)
--- src/controllers/Usuarios.js
+++ src/controllers/Usuarios.js
@@ -1,32 +1,39 @@
-import Validacoes from "../services/Validacoes.js"
-import Database from "../infra/Database.js"
-import UsuariosModel from "../models/UsuariosModel.js"
-import DatabaseMetodos from "../utils/DatabaseMetodos.js"
+// import Validacoes from "../services/Validacoes.js"
+// import Database from "../infra/Database.js"
+// import UsuariosModel from "../models/UsuariosModel.js"
+// import DatabaseMetodos from "../utils/DatabaseMetodos.js"
 
-class Usuarios{
-    static routers(app){
+// class Usuarios{
+//     static routers(app){
         
-        app.get("/usuarios", (req, res)=>{
-            if(Validacoes.validaNome("Diego")){
-                res.send(Database)
-            } else {
-                res.send("<h1>Deu Ruim!</h1>")
-            }
-        })
+//         app.get("/usuarios", (req, res)=>{
+//             if(Validacoes.validaNome("Diego")){
+//                 res.send(Database)
+//             } else {
+//                 res.send("<h1>Deu Ruim!</h1>")
+//             }
+//         })
 
-        app.get("/usuarios/:id", (req, res) => {
-            const id = req.params.id
-            const obj = Database[id]
-            console.log(Object.entries(obj))
-            res.status(200).json(obj.nome)
-        })
+//         app.get("/usuarios/:id", (req, res) => {
+//             // const id = req.params.id
+//             // const obj = Database[id]
+//             // console.log(Object.entries(obj))
+//             res.status(200).json(obj.nome)
+//         })
 
-        app.post("/usuarios", (req, res) =>{
-            const usuario = new UsuariosModel(...Object.values(req.body))
-            const resposta = DatabaseMetodos.iserir(usuario, Database)
-            res.status(201).json(resposta)
-        })
-    }
-}
+//         app.post("/usuarios", (req, res) =>{
+//             // const usuario = new UsuariosModel(...Object.values(req.body))
+//             // const resposta = DatabaseMetodos.iserir(usuario, Database)
+//             res.status(201).json(resposta)
+//         })
+//         app.put("/usuarios/:id", (req, res) => {
+//             res.status(200).json({resposta: "fiz um put"})
+//         })
+//         app.patch("/usuarios/:id", (req, res) => {
+//             res.status(200).json({resposta: "fiz um put"})
+//         })
+//     }
+// }
 
-export default Usuarios
+// export default Usuarios
+
