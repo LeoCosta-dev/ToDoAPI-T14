dcb3e49df97fe8e0e9d38af72c9dd8d4ed8ef8a3
Leonardo Costa <leonardodacostadasilva@hotmail.com>
2022-04-28 19:51:43 -0300

    subindo para a heroku

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import Usuarios from "./src/controllers/Usuarios.js"
+import cors from "cors"
 
 
 dotenv.config()
@@ -8,6 +9,7 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 3001
 
+app.use(cors())
 app.use(express.json())
 
 app.listen(port, ()=>{
